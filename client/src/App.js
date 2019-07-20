fa36cb256ab07f221c9a43c78ed58979fabf2a66
Tyson Hyland <tyhyland@hotmail.com>
2019-07-20 14:05:59 -0600

    department show page lists products

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Footer from './components/shared/Footer';
 import DepartmentsPage from './components/department/DepartmentsPage';
 import DepartmentShow from './components/department/DepartmentShow';
+import ProductShow from './components/product/ProductShow';
 
 const App = () => (
   <Fragment>
@@ -12,6 +13,7 @@ const App = () => (
     <Route exact path="/" component={Home} />
     <Route exact path="/DepartmentsPage" component={DepartmentsPage} />
     <Route exact path="/Department/:id" component={DepartmentShow} />
+    <Route exact path="/Product/:id" component={ProductShow} />
     </Switch>
 
   </div>
--- client/src/components/department/DepartmentShow.js
+++ client/src/components/department/DepartmentShow.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import axios from 'axios';
+import ProductList from '../product/ProductList';
 
 class DepartmentShow extends React.Component {
 
   state = { products: [] }
 
-  // component did mount here
+  componentDidMount() {
+    const { id } = this.props.location.state
+    axios.get(`/api/departments/${id}`)
+      .then( res => {
+        this.setState({ products: res.data })
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
 
   render() {
+    const { title, id } = this.props.location.state
+    const { products } = this.state
+
     return (
-      <h1>Individual department page</h1>
+      <>
+      <h1>Individual department page for { title }</h1>
+
+      <h1>All Products</h1>
+      <ul>
+        {
+          products.map( product => <ProductList key={product.id} {...product} /> )
+        }
+      </ul>
+      </>
     )
   }
 }
