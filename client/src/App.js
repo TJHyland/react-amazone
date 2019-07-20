2c15b37fc69ff71b10e6cb6432cb127dd55abf61
Tyson Hyland <tyhyland@hotmail.com>
2019-07-20 12:07:34 -0600

    department page and data seeding

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import DepartmentsPage from './components/department/DepartmentsPage';
 
 const App = () => (
   <Fragment>
   <div>
     <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/DepartmentsPage" component={DepartmentsPage} />
     </Switch>
 
   </div>
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+user = User.create({
+  email: 'user@email.com',
+  password: 'password'
+})
+
+departments = [1,2,3,4,5,6,7,8,9,10]
+
+10.times do
+  department = Department.create(
+    title: Faker::Food.dish,
+    user_id: user.id
+  )
+
+  10.times do
+    product = Product.create(
+      name: Faker::Food.fruits,
+      description: Faker::Food.description,
+      price: Faker::Number.digit,
+      stock: Faker::Number.digit,
+      department_id: departments.sample
+    )
+
+  end
+end
+puts "Data Seeded"
