0b4d5728e57e20421d8d5349020df70b4129addb
Tyson Hyland <tyhyland@hotmail.com>
2019-07-20 15:20:34 -0600

    minor update to crud

--- client/src/components/product/ProductShow.js
+++ client/src/components/product/ProductShow.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 class ProductShow extends React.Component {
 
+  render() {
+    const { id, name, description, price, stock, department_id } = this.props.location.state
+    return(
+      <>
+        <h1>Product show page for {name}</h1>
+        <p>item name: {name}</p>
+        <p>description: {description}</p>
+        <p>price: ${price}</p>
+        <p>stock: {stock}</p>
+      </>
+    )
+  }
+
 }
 
 export default ProductShow;
