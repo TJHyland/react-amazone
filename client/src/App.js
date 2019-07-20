ca9d1e7945bb49f7e0aa199395d5972cafd0c7bb
Kevin Nguyen <nguyen.kevin1991@gmail.com>
2019-07-20 14:13:50 -0600

    providers

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import NoMatch from './components/shared/NoMatch';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+
 
 const App = () => (
   <Fragment>
   <div>
-    <Switch>
-    <Route exact path="/" component={Home} />
-    </Switch>
-
+    <FetchUser>
+      <Switch>
+       <Route exact path="/" component={Home} />
+       <Route exact path='/login' component={Login} />
+       <Route exact path='/register' component={Register} />
+       <Route component={NoMatch} />
+      </Switch>
+    </FetchUser>
   </div>
   </Fragment>
 )
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { initMiddleware } from 'devise-axios';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider, } from './providers/AuthProvider';
 
 initMiddleware();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
   document.getElementById('root')
 )
