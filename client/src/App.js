e47bdc4564cc5b3d5c6bf6694a5ee145caf9a7a3
Ivanna Brooks <ibrooks@its.nicholls.edu>
2019-07-20 14:00:26 -0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/shared/Navbar';
 import Footer from './components/shared/Footer';
 import DepartmentsPage from './components/department/DepartmentsPage';
 
 const App = () => (
   <Fragment>
+    <Navbar />
   <div>
     <Switch>
     <Route exact path="/" component={Home} />
